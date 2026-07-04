160c6e1c42be7053b2454d2c83303b0e884f8f5a
Deborah Anyachukwu <108271834+zub-bee@users.noreply.github.com>
2026-07-04 21:11:14 +0100

    refactor(admin): streamline imports in AdminMinorUptakeQueryDto

--- src/modules/admin/engagement/dto/admin-minor-uptake-query.dto.ts
+++ src/modules/admin/engagement/dto/admin-minor-uptake-query.dto.ts
@@ -1,9 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsIn, IsOptional } from 'class-validator';
-import {
-  TALENT_ROLE_TRACKS,
-  TalentRoleTrack,
-} from '../../../talent/talent.constants';
+import { TALENT_ROLE_TRACKS } from '../../../talent/talent.constants';
+import type { TalentRoleTrack } from '../../../talent/talent.constants';
 
 export class AdminMinorUptakeQueryDto {
   @ApiProperty({ required: false, enum: TALENT_ROLE_TRACKS })
